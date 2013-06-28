a4da0b818494229be0efc7c74f5a01372d9d535d
Mandragorian <gkonstandinos@gmail.com>
2013-06-28 23:03:52 +0300

    first version

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+   
+   if(argc>1)
+   {
+      cout <<"is in the main section\n";
+      return 0;
+   }
+   else 
+   {
+      cout << "this will be the help section \n";
+   }
+}
