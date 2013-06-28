c1382623c650746e854f57b558a09bf85e64c217
Mandragorian <gkonstandinos@gmail.com>
2013-06-29 02:05:46 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <string>
+#include <calculum.h>
 
 using namespace std;
+
+
+
+
 
 int main(int argc, char* argv[])
 {
    
+   handler asd;
+
    if(argc>1)
    {
       cout <<"is in the main section\n";
+      asd.interface(argv);
       return 0;
    }
    else 
